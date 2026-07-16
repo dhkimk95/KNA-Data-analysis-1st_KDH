773091b5b7daa57d45f391fad5cfbd8744b9b90b
donghyung kim <dhkimk95@gmail.com>
2026-07-16 14:26:19 +0900

    input 실습2

--- 07_input copy.py
+++ 07_input copy.py
@@ -2,3 +2,15 @@ a = int(input("수1: "))
 b = int(input("수2: "))
 print("합 :", a + b)
 print("나누기 :", a / b)
+
+
+temp = int(input("온도: "))
+print("80 초과?", temp > 80)
+print("0 이상?", temp >= 0)
+
+x = int(input("점수1: "))
+y = int(input("점수2: "))
+z = int(input("점수3: "))
+avg = (x + y + z) / 2
+print(avg)
+print("60 이상?", avg >= 60)
